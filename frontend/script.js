b98def0d02a8fb34d0a12ef20569c9762ed26fbc
SistlaVishva2004 <vishnuvamsi04@gmail.com>
2025-05-12 15:12:17 +0530

    fetching

--- frontend/script.js
+++ frontend/script.js
@@ -21,7 +21,7 @@ async function sendMessageToBackend(userMessage) {
     try {
         console.log("📨 Sending request to backend...");
 
-        const response = await fetch("http://127.0.0.1:5000/chat", {
+        const response = await fetch("https://moviecharacters-bot.onrender.com/chat", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
